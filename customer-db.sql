10c2aac1b6e5ffea784d53444f29726f8200de02
MalkitShah <malkitsshah@gmail.com>
2022-08-22 12:25:40 +0100

    initial change

---
+++ customer-db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE customers;
+use customers; 
+CREATE TABLE CUSTOMER (
+id INT NOT NULL UNIQUE AUTO_INCREMENT,
+title  VARCHAR(10) NOT NULL,
+first_name VARCHAR(200) NOT NULL,
+last_name VARCHAR(200) NOT NULL,
+phone_number INT NOT NULL,
+postcode VARCHAR(20) NOT NULL,
+primary key(id));
+
+
+
+
+
+
+
+)
